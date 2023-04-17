13cf349694768ed6633aa905470214b72c367b05
Chris Cameron <kevlar.eq@gmail.com>
2023-04-16 23:40:01 -0400

    Modularized the script as much as possible. Created a modules folder and added url processing, web service, and mac service files.

--- error_handler.py
+++ error_handler.py
@@ -1,5 +1,6 @@
-#This script is an error handler and suggestion module intended to speed the process of debugging and writing useful code
-#It uses GPT-3.5 to parse errors and determine how to solve problems.
+# This script is an error handler and suggestion module intended to speed the process of debugging and writing useful code
+# It uses the OpenAI API to parse errors and determine how to solve problems. 
+# Currently testing with the Davinci Codex engine.
 
 import os
 import openai
@@ -12,7 +13,7 @@ load_dotenv()
 openai.organization = os.getenv("OPEN_AI_ORGANIZATION")
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-def ask_gpt3(error_description):
+def ask_gpt(error_description):
     prompt = f"An error occurred in my Python script: {error_description}. Can you provide a brief summary and a couple of possible solutions?"
 
     response = openai.Completion.create(
--- le.py
+++ le.py
@@ -1,75 +1,11 @@
 import csv
-import json
 import re
-import requests
-import urllib.parse
-from bs4 import BeautifulSoup
-from gpt3_error_handler import ask_gpt3
-from utils import load_anim
 import threading
+from error_handler import ask_gpt
+from modules.url_processing import expand_url, truncate_url
+from utils import load_anim
 
-def expand_url(short_url):
-    # Sends a GET request to the shortened URL and follows redirects using httpstatus API
-    headers = {'Content-Type': 'application/json'}
-    data = {'requestUrl': short_url}
-    response = requests.post('https://api.httpstatus.io/v1/status', headers=headers, data=json.dumps(data))
-    if response.status_code == 200:
-        try:
-            result = json.loads(response.content)
-            chain = result['response']['chain']
-            expanded_url = chain[-1]['url']
 
-            # Send a request to the expanded URL and parse the HTML content
-            page_response = requests.get(expanded_url)
-            if page_response.status_code == 200:
-                soup = BeautifulSoup(page_response.content, 'html.parser')
-                title_tag = soup.find('title')
-                if title_tag:
-                    title = title_tag.text.strip()
-                else:
-                    title = ''
-                    print(f"Warning: Title tag is missing in the HTML content for URL {expanded_url}.")
-            else:
-                title = ''
-                print(f"Warning: Unable to retrieve the HTML content for URL {expanded_url}. HTTP {page_response.status_code}")
-
-            return expanded_url, title
-        except Exception as e:
-            error_description = f"Error expanding URL: {short_url} - {str(e)}"
-            print(error_description)
-            gpt3_suggestions = ask_gpt3(error_description)
-            print("GPT-3 suggestions:")
-            print(gpt3_suggestions)
-            return None, None
-    else:
-        print(f"Error expanding URL: {short_url} - HTTP {response.status_code}")
-        print(f"JSON response: {response.content.decode('utf-8')}")
-        return None, None
-
-def truncate_url(expanded_url):
-    # Parses the expanded URL to extract components
-    parsed_url = urllib.parse.urlparse(expanded_url)
-
-    # Handles YouTube links
-    if 'youtube.com' in parsed_url.netloc or 'youtu.be' in parsed_url.netloc:
-        # Parses the query parameters
-        query_params = urllib.parse.parse_qs(parsed_url.query)
-
-        # Handles YouTube videos
-        video_id = query_params.get('v', None)
-        if video_id:
-            return f"https://www.youtube.com/watch?v={video_id[0]}"
-
-        # Handles YouTube playlists
-        playlist_id = query_params.get('list', None)
-        if playlist_id:
-            return f"https://www.youtube.com/playlist?list={playlist_id[0]}"
-
-        return expanded_url
-
-    # Truncates non-YouTube URLs by removing query strings
-    pattern = re.compile(r'(\?.*|\/\?.*)')
-    return pattern.sub('', expanded_url)
 
 def output_to_csv(urls):
     # Outputs the URLs to a CSV file
